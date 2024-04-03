fda614eca6f478d0858f69165794c7ff49f871cb
hvamsiprakash <143592103+hvamsiprakash@users.noreply.github.com>
2024-04-03 21:51:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -338,6 +338,40 @@ import numpy as np
 from keras.preprocessing import image as kp_image
 from keras.models import load_model as keras_load_model
 
+# New dictionary
+map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon', 2: 'bart_simpson', 
+        3: 'charles_montgomery_burns', 4: 'chief_wiggum', 5: 'comic_book_guy', 6: 'edna_krabappel', 
+        7: 'homer_simpson', 8: 'kent_brockman', 9: 'krusty_the_clown', 10: 'lisa_simpson', 
+        11: 'marge_simpson', 12: 'milhouse_van_houten', 13: 'moe_szyslak', 
+        14: 'ned_flanders', 15: 'nelson_muntz', 16: 'principal_skinner', 17: 'sideshow_bob'}
+
+# New functions
+def center_crop(img, dim):
+    """Returns center cropped image
+    Args:
+    img: image to be center cropped
+    dim: dimensions (width, height) to be cropped
+    """
+    width, height = img.shape[1], img.shape[0]
+
+    # process crop width and height for max available dimension
+    crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
+    crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
+    mid_x, mid_y = int(width/2), int(height/2)
+    cw2, ch2 = int(crop_width/2), int(crop_height/2) 
+    crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+    return crop_img
+
+# New pre-processing function
+def preprocess_img(request):
+    #f = request.files['file']
+    file_path = get_file_path_and_save(request)  # Assuming get_file_path_and_save is defined somewhere
+    image = cv2.imread(file_path)  # Assuming cv2 is imported somewhere
+    cropped_img = center_crop(image, image.shape)
+    pic = cv2.resize(cropped_img, (64,64))
+    img_reshape = pic.reshape(1, 64, 64, 3)
+    return img_reshape
+
 # Function to load and preprocess the image
 def preprocess_image(image):
     img = image.resize((64, 64))
@@ -377,10 +411,7 @@ def main():
         img = preprocess_image(image)
 
         # Make predictions
-        class_names = ['abraham_grampa_simpson', 'apu_nahasapeemapetilon', 'bart_simpson', 'charles_montgomery_burns',
-                       'chief_wiggum', 'comic_book_guy', 'edna_krabappel', 'homer_simpson', 'kent_brockman',
-                       'krusty_the_clown', 'lisa_simpson', 'marge_simpson', 'milhouse_van_houten', 'moe_szyslak',
-                       'ned_flanders', 'nelson_muntz', 'principal_skinner', 'sideshow_bob']
+        class_names = list(map_characters.values())
 
         # Prediction
         prediction = model.predict(img)
