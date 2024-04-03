98913c0119a18c633740db38d2b51ff0a9030e74
hvamsiprakash <143592103+hvamsiprakash@users.noreply.github.com>
2024-04-03 20:59:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -333,50 +333,10 @@
 #     st.write(f"Predicted Character: {predicted_character}")
 
 
-
 import streamlit as st
 from PIL import Image
 import numpy as np
-import tensorflow as tf
-
-# Function to load the model and class names
-@st.cache
-def load_model_and_labels():
-    model = tf.keras.models.load_model('models/model.h5')
-    
-    # Recompile the model to ensure its readiness
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    
-    class_names = {}
-    with open('models/labels.txt', 'r') as f:
-        for line in f:
-            try:
-                key, val = line.strip().split(' ', 1)
-                class_names[int(key)] = val.replace("_", " ")  # Replacing underscores with spaces
-            except ValueError:
-                # Skip lines that don't contain the expected format
-                pass
-    return model, class_names
-
-# Function to preprocess image
-def preprocess_image(image):
-    # Convert the image to RGB if it's not already
-    if image.mode != 'RGB':
-        image = image.convert('RGB')
-    
-    # Resize the image
-    img = image.resize((64, 64))
-    
-    # Convert image to numpy array
-    img = np.array(img)
-    
-    # Normalize image
-    img = img.astype("float32") / 255.0
-    
-    # Add batch dimension
-    img = np.expand_dims(img, axis=0)
-    
-    return img
+from utils import load_model_and_labels, preprocess_image
 
 # Load model and class names
 model, class_names = load_model_and_labels()
@@ -399,3 +359,4 @@ if uploaded_file is not None:
 
     # Display predicted character
     st.write(f"Predicted Character: {predicted_character}")
+
